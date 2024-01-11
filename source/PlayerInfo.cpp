29be8e99bc64779d084ecf180e68c53b08b05762
Timothy Collett <tcollett@topazgryphon.org>
2024-01-11 14:31:46 -0500

    refactor(mechanics): Move the actual iteration through mission timers to step them into a new Mission method, to avoid having mutual includes between PlayerInfo and Timer

--- source/PlayerInfo.cpp
+++ source/PlayerInfo.cpp
@@ -4168,8 +4168,7 @@ void PlayerInfo::StepMissions(UI *ui)
 void PlayerInfo::StepMissionTimers(UI *ui)
 {
 	for(Mission &mission : missions)
-		for(Timer &timer : mission.Timers())
-			timer.Step(*this, ui, mission);
+		mission.StepTimers(*this, ui);
 }
 
 
